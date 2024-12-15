bb07dec21477199c863e2bda54868cad84a399cb
Brandon Nguyen <nguyen.brandon316@gmail.com>
2024-12-14 18:32:48 -0600

    Started Spring Initializr

---
+++ ticketing-project/src/test/java/com/fullstackpep/ticketing_project/TicketingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackpep.ticketing_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
