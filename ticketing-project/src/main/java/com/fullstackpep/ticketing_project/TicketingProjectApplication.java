652410b0bdbd0624c8ae0edd507087b5c3d19520
Brandon Nguyen <nguyen.brandon316@gmail.com>
2024-12-14 18:47:11 -0600

    Added beginning changes for spring portion

--- ticketing-project/src/main/java/com/fullstackpep/ticketing_project/TicketingProjectApplication.java
+++ ticketing-project/src/main/java/com/fullstackpep/ticketing_project/TicketingProjectApplication.java
@@ -2,6 +2,15 @@ package com.fullstackpep.ticketing_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//from spring project
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.boot.SpringApplication.run;
+
 
 @SpringBootApplication
 public class TicketingProjectApplication {
---
+++ ticketing-project/src/main/java/com/fullstackpep/ticketing_project/controller/TicketingController.java
@@ -0,0 +1,14 @@
+package com.fullstackpep.ticketing_project.controller;
+
+//import entities and services here
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//import java libraries as need here
+
+public class TicketingController {
+    
+}
