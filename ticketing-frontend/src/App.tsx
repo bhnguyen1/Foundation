afe6ce4caaaefdd989f900741ca568f304e29ad6
Brandon Nguyen <nguyen.brandon316@gmail.com>
2024-12-15 20:07:37 -0600

    put back public folder

--- ticketing-frontend/src/App.tsx
+++ ticketing-frontend/src/App.tsx
@@ -6,13 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <ComponentOne/> */}
-      {/* <EventsDemo/> */}
-      {/* <ListDemo/> */}
-      {/* <ParentComponent/> */}
-      {/* <UserProvider> */}
-      {/* </UserProvider> */}
-        
+        <h1>MyTicketingApp</h1>
     </div>
   );
 }
